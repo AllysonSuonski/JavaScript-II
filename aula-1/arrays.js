12a32249073bd5df9d64b565972ae4f85e79c4be
AllysonSuonski <moreira.allyson@escola.pr.gov.br>
2026-04-24 01:47:48 +0000

    atividade 1 finalizada

---
+++ aula-1/arrays.js
@@ -0,0 +1,50 @@
+const edGames = [
+  {
+  titulo: "Shadow Legends: Ascensão Sombria",
+  preco: 79
+  },
+  {
+    titulo: "Cyber Drift: Corrida do Futuro",
+    preco: 59
+  },
+  {
+    titulo: "Reino de Aether: A Guerra dos Cristais",
+    preco: 99
+  },
+  {
+    titulo: "Zombie Outbreak: Sobrevivência Total",
+    preco: 49
+  },
+  {
+    titulo: "Mystic Quest: Segredos Antigos",
+    preco: 69
+  }
+]
+
+const edJames = [
+  {
+    titulo: "Ecos do Amanhã Perdido",
+    preco: 39
+  {
+    titulo: "A Última Chama do Norte",
+    preco: 44
+  },
+  {
+    titulo: "Segredos Sob a Lua de Prata",
+    preco: 34
+  },
+  {
+    titulo: "O Código das Sombras",
+    preco: 42
+  },
+  {
+    titulo: "Entre Mundos e Destinos",
+    preco: 84
+  },
+  {
+    titulo: "Cidade de Vidro e Névoa",
+    preco: 37
+  }
+]
+
+module.exports = { edGames, edJames }
