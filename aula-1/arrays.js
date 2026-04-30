d2bc8114c6205a65af745b6a7b0097e87cc4396e
AllysonSuonski <moreira.allyson@escola.pr.gov.br>
2026-04-30 13:45:07 +0000

    atividade 2 finalizada

--- aula-1/arrays.js
+++ aula-1/arrays.js
@@ -1,49 +1,49 @@
 const edGames = [
   {
-  titulo: "Shadow Legends: Ascensão Sombria",
+  titulo: "uncharted",
   preco: 79
   },
   {
-    titulo: "Cyber Drift: Corrida do Futuro",
+    titulo: "Tomb rainden",
     preco: 59
   },
   {
-    titulo: "Reino de Aether: A Guerra dos Cristais",
+    titulo: "Devil May Cry",
     preco: 99
   },
   {
-    titulo: "Zombie Outbreak: Sobrevivência Total",
+    titulo: "mortal kombat",
     preco: 49
   },
   {
-    titulo: "Mystic Quest: Segredos Antigos",
+    titulo: "Nao sei dar nomes",
     preco: 69
   }
 ]
 
 const edJames = [
   {
-    titulo: "Ecos do Amanhã Perdido",
+    titulo: "nomes e nomes ",
     preco: 39
   },
   {
-    titulo: "A Última Chama do Norte",
+    titulo: "nome",
     preco: 44
   },
   {
-    titulo: "Segredos Sob a Lua de Prata",
+    titulo: "kaua é yag",
     preco: 34
   },
   {
-    titulo: "O Código das Sombras",
+    titulo: "orongotamgo santificado",
     preco: 42
   },
   {
-    titulo: "Entre Mundos e Destinos",
+    titulo: "hary póter",
     preco: 84
   },
   {
-    titulo: "Cidade de Vidro e Névoa",
+    titulo: "Divina Comedia",
     preco: 37
   }
 ]
---
+++ aula-2/arrays.js
@@ -0,0 +1,48 @@
+const listaLivros = [
+  {
+    titulo: "minezin",
+    preco: 11
+  },
+  {
+    titulo: "Roblox",
+    preco: 2
+  },
+  {
+    titulo: "pokopia",
+    preco: 157
+  },
+  {
+  titulo: "Shenanigans",
+  preco: 777
+  },
+  {
+    titulo: "Silksong",
+    preco: 68
+  },
+  {
+    titulo: "Hollow Khight",
+    preco: 155
+  },
+  {
+    titulo: "Celeste",
+    preco: 21
+  },
+  {
+    titulo: "Pokemon",
+    preco: 43
+  },
+  {
+    titulo: "Dishonored",
+    preco: 73
+  },
+  {
+    titulo: "Devil May Cry",
+    preco: 666
+  },
+  {
+    titulo: "The Last Of Us",
+    preco: 1
+  },
+]
+
+module.exports = listaLivros
